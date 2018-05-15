77b412d80a6934d35dac417e358a417ceba2065b
Guilherme <contatogkguilherme@gmail.com>
2018-05-15 07:05:49 -0300

    Finalização dos testes de Integração de sistema faltado apenas uma revisão final

---
+++ DonaLaura/DonaLaura.Integration.Tests/Features/Orders/OrderIntegrationSqlTests.cs
@@ -0,0 +1,326 @@
+﻿using DonaLaura.Application.Features.Orders;
+using DonaLaura.Application.Features.Products;
+using DonaLaura.Common.Tests.Base;
+using DonaLaura.Domain.Exceptions;
+using DonaLaura.Domain.Features.Orders;
+using DonaLaura.Domain.Features.Products;
+using DonaLaura.Infra.Data.Features.Orders;
+using DonaLaura.Infra.Data.Features.Products;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaLaura.Integration.Tests.Features.Orders
+{
+    [TestFixture]
+    public class OrderIntegrationSqlTests
+    {
+        private OrderService _orderService;
+        private OrderRepository _orderRepositopty;
+        private Product _product;
+
+        [SetUp]
+        public void Initialize()
+        {
+            BaseSqlTests.SeedDatabase();
+            _product = ObjectMother.GetProductOk();
+            _orderRepositopty = new OrderRepository();
+            _orderService = new OrderService(_orderRepositopty);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_SaveOrder_ShouldBeOk()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+
+            Order orderSaved = _orderService.Add(orderToSave);
+
+            Order resultadoEncontrado = _orderService.Get(orderSaved.Id);
+            orderSaved.Should().NotBeNull();
+            resultadoEncontrado.Id.Should().Be(orderSaved.Id);
+            orderSaved.Client.Should().Be(orderToSave.Client);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_SaveOrder_ClientNameNullOrEmpty_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Client = null;
+
+            Action comparation = () => _orderService.Add(orderToSave);
+
+            comparation.Should().Throw<OrderClientNullOrEmptyException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_SaveOrder_ClientNameShort_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Client = "ABC";
+
+            Action comparation = () => _orderService.Add(orderToSave);
+
+            comparation.Should().Throw<OrderClientNameShortException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_SaveOrder_ClientNameOverFlow_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Client = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa";
+
+            Action comparation = () => _orderService.Add(orderToSave);
+
+            comparation.Should().Throw<OrderClientNameOverFlowException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_SaveOrder_AmountZero_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Amount = 0;
+
+            Action comparation = () => _orderService.Add(orderToSave);
+
+            comparation.Should().Throw<OrderAmountZeroException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_SaveOrder_ProductNull_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.product = null;
+
+            Action comparation = () => _orderService.Add(orderToSave);
+
+            comparation.Should().Throw<OrderProductNullException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_UpdateOrder_ShouldBeOk()
+        {
+            _product.Id = 1;
+            Order orderToUpdate = ObjectMother.GetOrderOk(_product);
+            orderToUpdate.Id = 1;
+            orderToUpdate.Client = "Teste de Atualização";
+
+            Order orderSaved = _orderService.Update(orderToUpdate);
+
+            Order resultadoEncontrado = _orderService.Get(orderSaved.Id);
+            orderSaved.Should().NotBeNull();
+            resultadoEncontrado.Id.Should().Be(orderSaved.Id);
+            orderSaved.Client.Should().Be(orderToUpdate.Client);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_UpdateOrder_ClientNameNullOrEmpty_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Client = null;
+
+            Action comparation = () => _orderService.Update(orderToSave);
+
+            comparation.Should().Throw<OrderClientNullOrEmptyException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_UpdateOrder_ClientNameShort_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Client = "ABC";
+
+            Action comparation = () => _orderService.Update(orderToSave);
+
+            comparation.Should().Throw<OrderClientNameShortException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_UpdateOrder_ClientNameOverFlow_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Client = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa";
+
+            Action comparation = () => _orderService.Update(orderToSave);
+
+            comparation.Should().Throw<OrderClientNameOverFlowException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_UpdateOrder_AmountZero_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Amount = 0;
+
+            Action comparation = () => _orderService.Update(orderToSave);
+
+            comparation.Should().Throw<OrderAmountZeroException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_UpdateOrder_ProductNull_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.product = null;
+
+            Action comparation = () => _orderService.Update(orderToSave);
+
+            comparation.Should().Throw<OrderProductNullException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_DeleteOrder__ShouldBeOk()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Id = 1;
+
+            _orderService.Delete(orderToSave);
+
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_DeleteOrder_InvalidId_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Id = -1;
+
+            Action comparation = () => _orderService.Delete(orderToSave);
+
+            comparation.Should().Throw<IdentifierUndefinedException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_DeleteOrder_OrderNotFound_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Id = 2;
+
+            _orderService.Delete(orderToSave);
+
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+            productRetorns.First().Client.Should().Be("Compra de Teste");
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_GetAll__ShouldBeOk()
+        {
+            int id = 1;
+
+            var productRetorns = _orderService.GetAll();
+
+            productRetorns.Count().Should().Be(1);
+            productRetorns.First().Id.Should().Be(id);
+            productRetorns.First().Client.Should().Be("Compra de Teste");
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_GetAll__ShouldBeNull()
+        {
+            BaseSqlTests.Helper_RemoveOrder();
+
+            var productRetorns = _orderService.GetAll();
+
+            productRetorns.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_GetOrder_ShouldBeOk()
+        {
+            int id = 1;
+
+            Order orderFinded = _orderService.Get(id);
+
+            orderFinded.Should().NotBeNull();
+            orderFinded.Id.Should().Be(1);
+            orderFinded.Client.Should().Be("Compra de Teste");
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_GetOrder_InvalidId_ShouldBeFail()
+        {
+            var id = -1;
+
+            Action comparation = () => _orderService.Get(id);
+
+            comparation.Should().Throw<IdentifierUndefinedException>();
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_GetOrder_ShouldBeFail()
+        {
+            var id = 2;
+            Order orderReturns = _orderService.Get(id);
+
+            orderReturns.Should().BeNull();
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_GetByProduct_ShouldBeFail()
+        {
+            _product.Id = 1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Id = 1;
+
+            var listorders = _orderService.GetByProduct(orderToSave.product.Id);
+
+            listorders.Count().Should().Be(1);
+            listorders.First().Client.Should().Be("Compra de Teste");
+        }
+
+        [Test]
+        public void Order_TestSystemIntegrationSql_GetByProduct_InvalidId_ShouldBeFail()
+        {
+            _product.Id = -1;
+            Order orderToSave = ObjectMother.GetOrderOk(_product);
+            orderToSave.Id = -1;
+
+            Action comparation = () => _orderService.GetByProduct(orderToSave.product.Id);
+
+            comparation.Should().Throw<IdentifierUndefinedException>();
+            var productRetorns = _orderService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+    }
+}
---
+++ DonaLaura/DonaLaura.Integration.Tests/Features/Products/ProductIntegrationSqlTests.cs
@@ -0,0 +1,336 @@
+﻿using DonaLaura.Application.Features.Orders;
+using DonaLaura.Application.Features.Products;
+using DonaLaura.Common.Tests.Base;
+using DonaLaura.Domain.Exceptions;
+using DonaLaura.Domain.Features.Orders;
+using DonaLaura.Domain.Features.Products;
+using DonaLaura.Infra.Data.Features.Orders;
+using DonaLaura.Infra.Data.Features.Products;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaLaura.Integration.Tests.Features.Products
+{
+    [TestFixture]
+    public class ProductIntegrationSqlTests
+    {
+        private ProductService _productService;
+        private OrderService _orderService;
+        private ProductRepository _repository;
+        private OrderRepository _orderRepositopty;
+
+        [SetUp]
+        public void Initialize()
+        {
+            BaseSqlTests.SeedDatabase();
+
+            _orderRepositopty = new OrderRepository();
+            _repository = new ProductRepository();
+            _orderService = new OrderService(_orderRepositopty);
+            _productService = new ProductService(_repository, _orderService);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_SaveProduct_ShouldBeOk()
+        {
+            Product productToSave = ObjectMother.GetProductOk();
+
+            Product productSaved = _productService.Add(productToSave);
+
+            Product resultadoEncontrado = _productService.Get(productSaved.Id);
+            productSaved.Should().NotBeNull();
+            resultadoEncontrado.Id.Should().Be(productSaved.Id);
+            productSaved.Name.Should().Be(productToSave.Name);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_SaveProduct_NameNullOrEmpty_ShouldBeFail()
+        {
+            Product productToSave = ObjectMother.GetProductOk();
+            productToSave.Name = null;
+
+            Action comparation = () => _productService.Add(productToSave);
+
+            comparation.Should().Throw<ProductNameNullOrEmptyException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_SaveProduct_NameShort_ShouldBeFail()
+        {
+            Product productToSave = ObjectMother.GetProductOk();
+            productToSave.Name = "ABC";
+
+            Action comparation = () => _productService.Add(productToSave);
+
+            comparation.Should().Throw<ProductNameShortException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_SaveProduct_NameOverFlow_ShouldBeFail()
+        {
+            Product productToSave = ObjectMother.GetProductOk();
+            productToSave.Name = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa";
+
+            Action comparation = () => _productService.Add(productToSave);
+
+            comparation.Should().Throw<ProductNameOverFlowException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_SaveProduct_CostPriceOverFlow_ShouldBeFail()
+        {
+            Product productToSave = ObjectMother.GetProductOk();
+            productToSave.CostPrice = 2.00;
+            productToSave.SalePrice = 1.00;
+
+            Action comparation = () => _productService.Add(productToSave);
+
+            comparation.Should().Throw<ProductCostPriceOverFlow>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_SaveProduct_InvalidExpiration_ShouldBeFail()
+        {
+            Product productToSave = ObjectMother.GetProductOk();
+            productToSave.Manufacture = DateTime.Now.AddDays(+1);
+            productToSave.Expiration = DateTime.Now.AddDays(-1);
+
+            Action comparation = () => _productService.Add(productToSave);
+
+            comparation.Should().Throw<ProductExpirationInvalidException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_UpdateProduct_ShouldBeOk()
+        {
+            Product productToUpdate = ObjectMother.GetProductOk();
+            productToUpdate.Id = 1;
+            productToUpdate.Name = "teste";
+
+            Product productEdited = _productService.Update(productToUpdate);
+
+            productEdited.Should().NotBeNull();
+            productEdited.Name.Should().Be(productToUpdate.Name);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_UpdateProduct_InvalidId_ShouldBeFail()
+        {
+            Product productToUpdate = ObjectMother.GetProductOk();
+            productToUpdate.Id = -1;
+
+            Action comparation = () => _productService.Update(productToUpdate);
+
+            comparation.Should().Throw<IdentifierUndefinedException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_UpdateProduct_NameNullOrEmpty_ShouldBeFail()
+        {
+            Product productToUpdate = ObjectMother.GetProductOk();
+            productToUpdate.Name = null;
+
+            Action comparation = () => _productService.Update(productToUpdate);
+
+            comparation.Should().Throw<ProductNameNullOrEmptyException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_UpdateProduct_NameShort_ShouldBeFail()
+        {
+            Product productToUpdate = ObjectMother.GetProductOk();
+            productToUpdate.Name = "ABC";
+
+            Action comparation = () => _productService.Update(productToUpdate);
+
+            comparation.Should().Throw<ProductNameShortException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_UpdateProduct_NameOverFlow_ShouldBeFail()
+        {
+            Product productToUpdate = ObjectMother.GetProductOk();
+            productToUpdate.Name = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa";
+
+            Action comparation = () => _productService.Update(productToUpdate);
+
+            comparation.Should().Throw<ProductNameOverFlowException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_UpdateProduct_CostPriceOverFlow_ShouldBeFail()
+        {
+            Product productToUpdate = ObjectMother.GetProductOk();
+            productToUpdate.CostPrice = 2.00;
+            productToUpdate.SalePrice = 1.00;
+
+            Action comparation = () => _productService.Update(productToUpdate);
+
+            comparation.Should().Throw<ProductCostPriceOverFlow>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_UpdateProduct_InvalidExpiration_ShouldBeFail()
+        {
+            Product productToUpdate = ObjectMother.GetProductOk();
+            productToUpdate.Manufacture = DateTime.Now.AddDays(+1);
+            productToUpdate.Expiration = DateTime.Now.AddDays(-1);
+
+            Action comparation = () => _productService.Update(productToUpdate);
+
+            comparation.Should().Throw<ProductExpirationInvalidException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_DeleteProduct__ShouldBeOk()
+        {
+            BaseSqlTests.Helper_RemoveOrder();
+
+            Product productToDelete = ObjectMother.GetProductOk();
+            productToDelete.Id = 1;
+
+            _productService.Delete(productToDelete);
+
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_DeleteProduct_InvalidId_ShouldBeFail()
+        {
+            Product productToDelete = ObjectMother.GetProductOk();
+            productToDelete.Id = -1;
+
+            Action comparation = () => _productService.Delete(productToDelete);
+
+            comparation.Should().Throw<IdentifierUndefinedException>();
+            var listPosts = _productService.GetAll();
+            listPosts.Count().Should().Be(1);
+            listPosts.First().Id.Should().Be(1);
+            listPosts.First().Name.Should().Be("Produto de Teste");
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_DeleteProduct_ProductNotFound_ShouldBeFail()
+        {
+            Product productToDelete = ObjectMother.GetProductOk();
+            productToDelete.Id = 2;
+
+            _productService.Delete(productToDelete);
+
+            var listPosts = _productService.GetAll();
+            listPosts.Count().Should().Be(1);
+            listPosts.First().Id.Should().Be(1);
+            listPosts.First().Name.Should().Be("Produto de Teste");
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_GetAll__ShouldBeOk()
+        {
+            var listPosts = _productService.GetAll();
+
+            listPosts.Should().NotBeNull();
+            listPosts.Count().Should().Be(1);
+            listPosts.First().Id.Should().Be(1);
+            listPosts.First().Name.Should().Be("Produto de Teste");
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_GetAll__ShouldBeNull()
+        {
+            BaseSqlTests.Helper_RemoveOrder();
+            Product productToDelete = ObjectMother.GetProductOk();
+            productToDelete.Id = 1;
+            _productService.Delete(productToDelete);
+
+            var listPosts = _productService.GetAll();
+
+            listPosts.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_GetProduct__ShouldBeOk()
+        {
+            var id = 1;
+            Product productReturns = _productService.Get(id);
+
+            productReturns.Should().NotBeNull();
+            productReturns.Id.Should().Be(id);
+            productReturns.Name.Should().Be("Produto de Teste");
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_GetProduct_InvalidId_ShouldBeFail()
+        {
+            var id = -1;
+            Action comparation = () => _productService.Get(id);
+
+            comparation.Should().Throw<IdentifierUndefinedException>();
+
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_GetProduct__ShouldBeNull()
+        {
+            var id = 2;
+            Product productReturns = _productService.Get(id);
+
+            productReturns.Should().BeNull();
+        }
+
+
+        //Conferir com Guilherme ou Thiago...
+        [Test]
+        public void Product_TestSystemIntegrationSql_GetByProduct_ProductWithDependences_ShouldBeFail()
+        {
+            Product product = ObjectMother.GetProductOk();
+            product.Id = 1;
+ 
+            Action comparation = () => _productService.Delete(product);
+
+            comparation.Should().Throw<ProductWithDependecesException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Product_TestSystemIntegrationSql_GetByProduct_InvalidId_ShouldBeFail()
+        {
+            Product product = ObjectMother.GetProductOk();
+            product.Id = -1;
+
+            Action comparation = () => _productService.Delete(product);
+
+            comparation.Should().Throw<IdentifierUndefinedException>();
+            var productRetorns = _productService.GetAll();
+            productRetorns.Count().Should().Be(1);
+        }
+    }
+}
